e9789e524ad3e77d15c45042174b258d0ff8b785
Eyosiyas Gezahegn <eyosiyasgezahegn@gmail.com>
2025-02-25 23:53:35 +0300

    Generating html by js, then Fix price formating and radio selector

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart =[];
+export const cart =[
+  { 
+productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+ name: "Black and Gray Athletic Cotton Socks - 6 Pairs"
+},
+{
+  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  name: "Intermediate Size Basketball"
+}
+];
 
 export function addToCart(productId){
     let matchItem;
@@ -14,8 +23,9 @@ export function addToCart(productId){
   }
   else{
           cart.push({
-          productId:productId,
+          productId,
           quantity:1
           })
   }
+  console.log(cart);
   }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart,addToCart} from '../data/cart.js'
 import {products} from '../data/products.js'
+import {currencyFormat} from './utils/money.js'
 
 let productHTML='';
 
@@ -23,7 +24,7 @@ let productHTML='';
             </div>
   
             <div class="product-price">
-              $${(product.priceCents/100).toFixed(2)}
+              $${currencyFormat(product.priceCents)}
             </div>
   
             <div class="product-quantity-container">

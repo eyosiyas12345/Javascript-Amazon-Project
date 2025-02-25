38ac330835ee21126a7740079d8bfc0a60be7d27
Eyosiyas Gezahegn <eyosiyasgezahegn@gmail.com>
2025-02-25 16:11:30 +0300

    I started using module i.e: exporting and emproting files.

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
-const cart =[];
+export const cart =[];
+
+export function addToCart(productId){
+    let matchItem;
+  
+    cart.forEach((cartItem)=>{
+      if(productId===cartItem.productId){
+        matchItem=cartItem;
+      }
+    })
+  
+  if(matchItem){
+    matchItem.quantity++;
+  }
+  else{
+          cart.push({
+          productId:productId,
+          quantity:1
+          })
+  }
+  }
